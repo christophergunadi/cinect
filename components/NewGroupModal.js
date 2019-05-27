8af688d12fd27b8a9148da980377968bfc444796
estherwong777 <estherwong19@gmail.com>
2019-05-27 23:29:50 +0100

    [ehw4117 jlq17] Created new group form in modal

--- components/NewGroupModal.js
+++ components/NewGroupModal.js
@@ -1,5 +1,5 @@
 import React , {Component} from 'react';
-import {Dimensions, Text, View, StyleSheet, FlatList, Image} from 'react-native';
+import {Dimensions, Text, View, StyleSheet, FlatList, Image, TextInput,TouchableOpacity, Keyboard} from 'react-native';
 import Modal from 'react-native-modalbox';
 
 var windowSize = Dimensions.get('window');
@@ -13,6 +13,8 @@ export default class NewGroupModal extends Component {
   constructor(props) {
     super(props);
   }
+
+  _onAddMemberButton = () => {}
 
   render() {
     return (
@@ -30,21 +32,45 @@ export default class NewGroupModal extends Component {
         <View style={styles.container}>
           <Text style={styles.title}>Create a new group</Text>
 
-          <FlatList
-            data={[
-              {key: 'David'},
-              {key: 'Kate'},
-            ]}
-            renderItem={({item}) =>
-              <View style={{ flexDirection: 'row' }}>
-                <Image source={require('../assets/img/tempprofileicon.png')} style={styles.profileicon}/>
-                <Text style={styles.groupName}>{item.key}</Text>
-              </View>
-            }
-          />
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between'}}>
+            <TouchableOpacity onPress={() => alert("I want to set a DP")}>
+              <Image source={require('../assets/img/tempprofileicon.png')} style={styles.profileicon}/>
+            </TouchableOpacity>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Enter group name"
+              maxLength={15}
+              onBlur={Keyboard.dismiss}
+            />
+          </View>
+
+          <Text style={styles.subtitle}>Current members</Text>
+          <View>
+            <FlatList
+              data={[
+                {key: 'David'},
+                {key: 'Kate'},
+              ]}
+              renderItem={({item}) =>
+                <View style={{ flexDirection: 'row' }}>
+                  <Image source={require('../assets/img/tempprofileicon.png')} style={styles.profileicon}/>
+                  <Text style={styles.groupName}>{item.key}</Text>
+                </View>
+              }
+            />
+          </View>
+
+          <TouchableOpacity style={styles.addButton} onPress={this._onAddMemberButton}>
+            <Text style={styles.title}>+</Text>
+          </TouchableOpacity>
+
+          <View style={{ justifyContent: 'flex-end', alignItems: 'center', bottom: 0, flex: 1 }}>
+            <TouchableOpacity style={styles.createButton}>
+              <Text style={{ fontFamily: 'PT_Sans-Caption-Regular', color: '#000000' }}>Create</Text>
+            </TouchableOpacity>
+          </View>
 
         </View>
-
       </Modal>
     )
   }
@@ -64,17 +90,52 @@ const styles = StyleSheet.create({
   },
   groupName: {
     fontFamily: 'PT_Sans-Caption-Regular',
-    fontSize: 18,
+    fontSize: 15,
     color: '#000000',
     textAlign: 'left',
     marginLeft: 20,
     marginTop: 20,
   },
   profileicon: {
-    width: 50,
-    height: 50,
+    width: 45,
+    height: 45,
     borderRadius: 25,
     marginTop: 5,
     marginBottom: 5,
+  },
+  subtitle: {
+    fontFamily: 'PT_Sans-Caption-Regular',
+    fontSize: 17,
+    color: '#000000',
+    marginTop: 15,
+    marginBottom: 5,
+  },
+  textInput: {
+    width: windowSize.width - 220,
+    borderBottomColor: '#000000',
+    borderBottomWidth: 1,
+    marginRight: 20,
+    paddingBottom: -10,
+    marginBottom: 10,
+  },
+  createButton: {
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#5ac4e8',
+    height: 30,
+    width: windowSize.width / 3,
+  },
+  addButton: {
+    borderWidth: 1,
+    borderColor: '#000000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 45,
+    height: 45,
+    backgroundColor: '#fff',
+    borderRadius: 25,
+    marginRight: 30,
+    marginTop: 5,
   },
 })
