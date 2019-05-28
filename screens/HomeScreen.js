e5ac10272b8ac819c7c2d911251ea353831538f1
Christopher Gunadi <christopher.gunadi17@imperial.ac.uk>
2019-05-28 21:33:46 +0100

    Started working on swiping feature in homepage

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,51 @@
 import React, {Component} from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View, Animated, Dimensions, Image, PanResponder } from 'react-native';
+
+const SCREEN_HEIGHT = Dimensions.get('window').height;
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+const Movies = [
+  { uri: require('../assets/movieposters/movie1.jpg') },
+  { uri: require('../assets/movieposters/movie2.jpg') },
+  { uri: require('../assets/movieposters/movie3.jpg') },
+]
 
 export default class HomeScreen extends React.Component {
+
+  // componentWillMount() {
+  //   this.PanResponder = PanResponder.create({
+  //     onStart
+  //   })
+  // }
+
+  renderMovies = () => {
+
+    return Movies.map((item, i) => {
+      return (
+        <Animated.View key={i} 
+          style={{ height: SCREEN_HEIGHT - 120, width: SCREEN_WIDTH, padding: 10, position: 'absolute'}}>
+          <Image
+            style={{ flex: 1, height: null, width: null, resizeMode: 'cover', 
+            borderRadius: 20 }}
+            source={item.uri} />
+        </Animated.View>
+      )
+    }).reverse()
+
+    
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Groups"
-          onPress={() => this.props.navigation.navigate('Groups')}
-        />
-        <Button
-          title="Movies"
-          onPress={() => this.props.navigation.navigate('Movies')}
-        />
+      <View style={{ flex: 1 }}>
+
+        <View style={{ flex: 1 }}>
+          {this.renderMovies()}
+        </View>
+
+        <View style={{ height: 60 }}>
+
+        </View>
       </View>
     );
   }
