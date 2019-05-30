c7201532c3b4f4b16d4a65f3da8a4763580b4584
jasminequah <jasminequah@outlook.com>
2019-05-30 16:36:35 +0100

    Remove cookie restriction from POST requests

--- backend/cinect_api/urls.py
+++ backend/cinect_api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-# from cinect_api.views import UserViewSet
-# from rest_framework.routers import DefaultRouter
+from django.views.decorators.csrf import csrf_exempt
 
 from . import views
 
@@ -8,5 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('user', views.user, name='user'),
     path('suggest', views.groupSuggestion, name='suggest'),
-    path('addswipedright', views.addSwipedRight, name='addswipedright')
+    path('addswipedright', csrf_exempt(views.addSwipedRight), name='addswipedright')
 ]
