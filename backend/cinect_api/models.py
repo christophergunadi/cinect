015dccef92b7af5a7fe0e73f391de8341cbc84f9
Christopher Gunadi <christopher.gunadi17@imperial.ac.uk>
2019-05-30 11:33:57 +0100

    [cg3417] Minor improvement to models.py and tweaked settings.py

--- backend/cinect_api/models.py
+++ backend/cinect_api/models.py
@@ -4,6 +4,8 @@ class User(models.Model):
   username = models.CharField(max_length=20, primary_key=True)
   class Meta:
     db_table = 'users'
+  def __str__(self):
+    return self.username
 
 class SwipedRight(models.Model):
   swiperightid = models.IntegerField(default=-1, primary_key=True)
@@ -11,5 +13,6 @@ class SwipedRight(models.Model):
   imdbid = models.CharField(max_length=10)
   class Meta:
     db_table = 'swipedright'
-
+  def __str__(self):
+    return f'{self.username}, {self.imdbid}'
 # Create your models here.
