9e1f0a88b4a96ae04ee8d056da566b7d7b92a7ff
jasminequah <jasminequah@outlook.com>
2019-05-31 02:20:22 +0100

    Add tests to check status code of HTTP requests to /user and /addswipedright

--- backend/cinect_api/tests.py
+++ backend/cinect_api/tests.py
@@ -1,13 +1,24 @@
 from django.test import SimpleTestCase
 from .views import user
 from django.test.client import RequestFactory
+from django.urls import reverse
 
 # Create your tests here.
 class ExternalAPITest(SimpleTestCase):
     def setUp(self):
-      self.factory = RequestFactory()
+        self.factory = RequestFactory()
 
     def test_if_can_call_external_api(self):
-      request = self.factory.get('/user?username=Alexander')
-      self.assertIsNotNone(user(request))
+        request = self.factory.get('/user?username=Alexander')
+        self.assertIsNotNone(user(request))
+
+class CinectAPITest(SimpleTestCase):
+    def test_user_status_code(self):
+        response = self.client.get(reverse('user'))
+        self.assertEquals(response.status_code, 200)
     
+    def test_swiperight_status_code(self):
+        response = self.client.get(reverse('addswipedright'))
+        self.assertEquals(response.status_code, 200)
+    
+   
