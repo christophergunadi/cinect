e88279ffe46ef358429c79fa5847b065aec8887b
jasminequah <jasminequah@outlook.com>
2019-06-05 13:41:08 +0100

    Include functionality to see which streaming services film available on (commented out to save API usage)

---
+++ frontend/components/OriginalSizeImage.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {View, Image} from 'react-native';
+
+export default class OriginalSizeImage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: 0,
+      height: 0,
+    }
+    this.getOriginalSize = this.getOriginalSize.bind(this);
+  }
+
+  getOriginalSize = () => {
+    Image.getSize(this.props.source, (srcWidth, srcHeight) => {
+      this.setState({
+        width: srcWidth,
+        height: srcHeight,
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.getOriginalSize();
+  }
+
+  render() {
+    // alert(this.state.width + "//" + this.state.height);
+    return (
+      <View>
+        <Image source={{uri: this.props.source}} style={{width: this.state.width, height: this.state.height, margin: 10}}/>
+      </View>
+    );
+  }
+}
--- frontend/screens/GroupMovieScreen.js
+++ frontend/screens/GroupMovieScreen.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import {Text, View, Image, Button, Dimensions, TouchableOpacity, ScrollView, StyleSheet} from 'react-native';
+import {Text, View, Image, Button, Dimensions, Linking, TouchableOpacity, ScrollView, StyleSheet} from 'react-native';
+
+import OriginalSizeImage from '../components/OriginalSizeImage';
 
 import MainStylesheet from '../styles/MainStylesheet';
 
@@ -9,8 +11,51 @@ export default class GroupMovieScreen extends React.Component {
         let {width, height} = Dimensions.get('window')
         this.state = {
             width: '100%',
-            height: '80%'
+            height: '80%',
+            showingOn: [],
         }
+    }
+
+    componentDidMount() {
+      this.getStreamingAvailability()
+    }
+
+    getStreamingAvailability = () => {
+      // fetch("https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term="+ this.props.navigation.getParam('title') +"&country=uk", {
+      //   headers: new Headers({
+      //     'X-RapidAPI-Host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com',
+      //     'X-RapidAPI-Key':'10cd8edd93msh174b0f61d4ea30dp1361a6jsn1dddee813f67'
+      //   })
+      // })
+      // .then(response => response.json())
+      // .then((responseJson) => {
+      //   // TODO: Check movie name matches?
+      //   if (responseJson.results[0].name == this.props.navigation.getParam('title')) {
+      //     sites = responseJson.results[0].locations
+      //     for (let i = 0; i < sites.length; i++) {
+      //       this.setState({
+      //         showingOn: [...this.state.showingOn, {
+      //           url: sites[i].url,
+      //           logo: sites[i].icon,
+      //         }]
+      //       })
+      //     }
+      //   }
+      //   // DEBUG
+      //   console.log(responseJson.results);
+      // });
+      this.setState({
+        showingOn: [...this.state.showingOn, {
+          url: 'https://www.netflix.com/title/80095314',
+          logo: 'https://utellyassets7.imgix.net/locations_icons/utelly/black_new/Netflix.png?auto=compress&app_version=2977d84b-593d-4167-a904-57b1e6e814f1_2019-06-04&w=92',
+        }, {
+          url: 'NULL',
+          logo: 'https://utellyassets7.imgix.net/locations_icons/utelly/black_new/Amazon.png?auto=compress&app_version=2977d84b-593d-4167-a904-57b1e6e814f1_2019-06-04&w=92',
+        }, {
+          url: 'NULL',
+          logo: 'http://utellyassets7.imgix.net/locations_icons/utelly/live_tv_square/50d352abf0ca9f5798000383.jpg?auto=compress&app_version=2977d84b-593d-4167-a904-57b1e6e814f1_2019-06-04&w=92',
+        }]
+      })
     }
 
     pressDeleteButton() {
@@ -19,13 +64,12 @@ export default class GroupMovieScreen extends React.Component {
 
     render() {
         return (
-        //   <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <View style={MainStylesheet.container}>
-          <ScrollView style={{height: '100%', width: '100%'}}>
+          <ScrollView style={{height: '100%', width: '103%'}}>
             {/* <View> */}
+            {/* TODO: Change width and height*/}
                 <Image source={{uri: "http://image.tmdb.org/t/p/w500/" + this.props.navigation.getParam('posterpath')}}
-                // <Image source={{uri: "http://image.tmdb.org/t/p/w500/" + this.props.navigation.getParam('posterpath')}}
-                    style={{width: 300, height: 500, borderRadius: 10}}/>
+                    style={{width: 330, height: 500, borderRadius: 10}}/>
                 <Text style={MainStylesheet.title}>
                     {this.props.navigation.getParam('title')}
                 </Text>
@@ -37,18 +81,27 @@ export default class GroupMovieScreen extends React.Component {
                         <Text>Send movie invite</Text>
                     </TouchableOpacity>
                 </View>
-                <Text style={{fontWeight: 'bold'}}>Synopsis:{'\n'}</Text>
+                <Text style={{fontWeight: 'bold'}}>Synopsis:{"\n"}</Text>
                 <Text>
-                {this.props.navigation.getParam('synopsis')}
-                {'\n'}
+                  {this.props.navigation.getParam('synopsis')}
+                  {"\n"}
                 </Text>
                 <Text style={{fontWeight: 'bold'}}>Available on:</Text>
-                <View>
-                  {/* // {this.state.showingOn.map(site => {
-                  //   if site.url !== NULL
-                  // })} */}
-                  <Image source={{uri: "https://utellyassets7.imgix.net/locations_icons/utelly/black_new/Netflix.png?auto=compress&app_version=2977d84b-593d-4167-a904-57b1e6e814f1_2019-06-04&w=92"}}
-                    style={{width: 100, height: 30, padding: 20}}/>
+                <View style={{flex: 1, flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center'}}>
+                  {this.state.showingOn.map(site => {
+
+                    if (site.url == null) {
+                      return (
+                        <TouchableOpacity onPress={() => Linking.openURL(site.url).catch(error => alert(error))}>
+                          <OriginalSizeImage source={site.logo}/>
+                          {/* <Image source={{uri: site.logo}} style={{width: imgWidth, height: imgHeight, padding: 20}}/> */}
+                        </TouchableOpacity>
+                      )
+                    } else {
+                      return (<OriginalSizeImage source={site.logo}/>)
+                      {/* return (<Image source={{uri: site.logo}} style={{width: imgWidth, height: imgHeight, padding: 20}}/>) */}
+                    }
+                  })}
                 </View>
             {/* </View> */}
 
@@ -75,5 +128,5 @@ const styles = StyleSheet.create({
         backgroundColor: 'skyblue',
         height: 35,
         width: Dimensions.get('window').width / 3,
-      },
+    },
 });
