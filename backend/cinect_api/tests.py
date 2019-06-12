9b7766aae6553080b79056568996a595d61a90fc
Ashly Lau <ashlylau@gmail.com>
2019-06-12 16:34:16 +0100

    Modified tests to match newly defined API calls

--- backend/cinect_api/tests.py
+++ backend/cinect_api/tests.py
@@ -4,19 +4,8 @@ from django.test.client import RequestFactory
 from django.urls import reverse
 
 # Create your tests here.
-class ExternalAPITest(SimpleTestCase):
-    def setUp(self):
-        self.factory = RequestFactory()
-
-    def test_if_can_call_external_api(self):
-        request = self.factory.get('/getmovies?email=group19@ic.ac.uk')
-        self.assertIsNotNone(getMoviesForUser(request))
 
 class CinectAPITest(SimpleTestCase):
-    def test_user_status_code(self):
-        response = self.client.get(reverse('getmovies'))
-        self.assertEquals(response.status_code, 200)
-    
     def test_swiperight_status_code(self):
         response = self.client.get(reverse('addswipedright'))
         self.assertEquals(response.status_code, 200)
