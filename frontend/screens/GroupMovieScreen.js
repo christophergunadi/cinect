4ef17512c0508656bb790792270d78491a300023
jasminequah <jasminequah@outlook.com>
2019-06-16 13:52:07 +0100

    Uncommented external Utelly API usage

--- frontend/screens/GroupMovieScreen.js
+++ frontend/screens/GroupMovieScreen.js
@@ -16,7 +16,8 @@ export default class GroupMovieScreen extends React.Component {
             showingOn: [],
             members: [],
         }
-        this.pressWatched = this.pressWatched.bind(this)
+        this.pressWatched = this.pressWatched.bind(this);
+        this.renderStreamingAvailability = this.renderStreamingAvailability.bind(this);
     }
 
     componentDidMount() {
@@ -25,45 +26,70 @@ export default class GroupMovieScreen extends React.Component {
     }
 
     getStreamingAvailability = () => {
-      // fetch("https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term="+ this.props.navigation.getParam('title') +"&country=uk", {
-      //   headers: new Headers({
-      //     'X-RapidAPI-Host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com',
-      //     'X-RapidAPI-Key':'10cd8edd93msh174b0f61d4ea30dp1361a6jsn1dddee813f67'
-      //   })
-      // })
-      // .then(response => response.json())
-      // .then((responseJson) => {
-      //   // TODO: Check movie name matches?
-      //   if (responseJson.results[0].name == this.props.navigation.getParam('title')) {
-      //     sites = responseJson.results[0].locations
-      //     for (let i = 0; i < sites.length; i++) {
-      //       this.setState({
-      //         showingOn: [...this.state.showingOn, {
-      //           url: sites[i].url,
-      //           logo: sites[i].icon,
-      //         }]
-      //       })
-      //     }
-      //   }
-      //   // DEBUG
-      //   console.log(responseJson.results);
-      // });
-      this.setState({
-        showingOn: [...this.state.showingOn, {
-          url: 'https://www.netflix.com/title/80095314',
-          logo: 'https://utellyassets7.imgix.net/locations_icons/utelly/black_new/Netflix.png?auto=compress&app_version=2977d84b-593d-4167-a904-57b1e6e814f1_2019-06-04&w=92',
-        }, {
-          url: null,
-          logo: 'https://utellyassets7.imgix.net/locations_icons/utelly/black_new/Amazon.png?auto=compress&app_version=2977d84b-593d-4167-a904-57b1e6e814f1_2019-06-04&w=92',
-        }, {
-          url: null,
-          logo: 'http://utellyassets7.imgix.net/locations_icons/utelly/live_tv_square/50d352abf0ca9f5798000383.jpg?auto=compress&app_version=2977d84b-593d-4167-a904-57b1e6e814f1_2019-06-04&w=92',
-        }]
+      fetch("https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term="+ this.props.navigation.getParam('title') +"&country=uk", {
+        headers: new Headers({
+          'X-RapidAPI-Host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com',
+          'X-RapidAPI-Key':'10cd8edd93msh174b0f61d4ea30dp1361a6jsn1dddee813f67'
+        })
       })
+      .then(response => response.json())
+      .then((responseJson) => {
+        if (responseJson.results[0].name == this.props.navigation.getParam('title')) {
+          sites = responseJson.results[0].locations
+          for (let i = 0; i < sites.length; i++) {
+            this.setState({
+              showingOn: [...this.state.showingOn, {
+                url: sites[i].url,
+                logo: sites[i].icon,
+              }]
+            })
+          }
+        }
+        // DEBUG
+        console.log(responseJson.results);
+      });
+      // this.setState({
+      //   showingOn: [...this.state.showingOn, {
+      //     url: 'https://www.netflix.com/title/80095314',
+      //     logo: 'https://utellyassets7.imgix.net/locations_icons/utelly/black_new/Netflix.png?auto=compress&app_version=2977d84b-593d-4167-a904-57b1e6e814f1_2019-06-04&w=92',
+      //   }, {
+      //     url: null,
+      //     logo: 'https://utellyassets7.imgix.net/locations_icons/utelly/black_new/Amazon.png?auto=compress&app_version=2977d84b-593d-4167-a904-57b1e6e814f1_2019-06-04&w=92',
+      //   }, {
+      //     url: null,
+      //     logo: 'http://utellyassets7.imgix.net/locations_icons/utelly/live_tv_square/50d352abf0ca9f5798000383.jpg?auto=compress&app_version=2977d84b-593d-4167-a904-57b1e6e814f1_2019-06-04&w=92',
+      //   }]
+      // })
     }
 
     pressDeleteButton() {
 
+    }
+
+    renderStreamingAvailability = () => {
+      if (Object.keys(this.state.showingOn).length === 0) {
+        return (
+          <View style={{paddingBottom: 13}}>
+            <Text>No streaming availability at this time.</Text>
+          </View>
+        )
+      } else {
+        return (
+          <View style={{flex: 1, flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center', paddingBottom: 13}}>
+            {this.state.showingOn.map(site => {
+              if (site.url !== null) {
+                return (
+                  <TouchableOpacity onPress={() => Linking.openURL(site.url).catch(error => alert(error))}>
+                    <OriginalSizeImage source={site.logo}/>
+                  </TouchableOpacity>
+                )
+              } else {
+                return (<OriginalSizeImage source={site.logo}/>)
+              }
+          })}
+          </View>
+        )
+      }
     }
 
     pressWatched() {
@@ -88,22 +114,15 @@ export default class GroupMovieScreen extends React.Component {
           paddingRight: 30,
           paddingBottom: 30,
           backgroundColor: 'transparent',}}>
-            
+
           <ScrollView style={{height: '100%', width: '103%'}}>
-            {/* <View> */}
             {/* TODO: Change width and height*/}
                 <Image source={{uri: "http://image.tmdb.org/t/p/w500/" + this.props.navigation.getParam('posterpath')}}
                     style={{width: 330, height: 500, borderRadius: 10}}/>
                 <Text style={MainStylesheet.title}>
                     {this.props.navigation.getParam('title')}
                 </Text>
-            {/* </View> */}
-
-            {/* <View style={{flex:2, justifyContent: "flex-end"}}> */}
                 <View style={{flex:1, flexDirection:'row', justifyContent: 'space-evenly'}}>
-                    {/* <TouchableOpacity style={styles.watchedButton}>
-                        <Text>Send movie invite</Text>
-                    </TouchableOpacity> */}
                 </View>
                 <Text style={styles.movietitleStyle}>Synopsis:{"\n"}</Text>
                 <Text>
@@ -111,22 +130,7 @@ export default class GroupMovieScreen extends React.Component {
                   {"\n"}
                 </Text>
                 <Text style={styles.movietitleStyle}>Available on:</Text>
-                <View style={{flex: 1, flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center'}}>
-                  {this.state.showingOn.map(site => {
-                    if (site.url !== null) {
-                      return (
-                        <TouchableOpacity onPress={() => Linking.openURL(site.url).catch(error => alert(error))}>
-                          <OriginalSizeImage source={site.logo}/>
-                          {/* <Image source={{uri: site.logo}} style={{width: imgWidth, height: imgHeight, padding: 20}}/> */}
-                        </TouchableOpacity>
-                      )
-                    } else {
-                      return (<OriginalSizeImage source={site.logo}/>)
-                      {/* return (<Image source={{uri: site.logo}} style={{width: imgWidth, height: imgHeight, padding: 20}}/>) */}
-                    }
-                  })}
-                </View>
-            {/* </View> */}
+                {this.renderStreamingAvailability()}
 
           </ScrollView>
           <LinearGradient locations={[0.85, 0.898]} colors={['transparent', '#FFFFFF']} style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0}}/>
--- frontend/screens/WatchlistMovieScreen.js
+++ frontend/screens/WatchlistMovieScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, View, Image, Button, Dimensions, TouchableOpacity, StyleSheet, TouchableWithoutFeedback} from 'react-native';
+import {Text, View, Image, Button, Dimensions, Linking, TouchableOpacity, StyleSheet, TouchableWithoutFeedback} from 'react-native';
 
 import {GetUserProperty} from '../Helpers';
 import OriginalSizeImage from '../components/OriginalSizeImage';
@@ -23,6 +23,7 @@ export default class WatchlistMovieScreen extends React.Component {
             movieRatings: [], // each rating is in the format {moviename: xx, stars: x, comment: xx}
             loading: true,
         }
+        this.renderStreamingAvailability = this.renderStreamingAvailability.bind(this);
     }
 
     componentDidMount() {
@@ -152,43 +153,68 @@ export default class WatchlistMovieScreen extends React.Component {
     }
 
     getStreamingAvailability = () => {
-        // fetch("https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term="+ this.props.navigation.getParam('title') +"&country=uk", {
-        //   headers: new Headers({
-        //     'X-RapidAPI-Host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com',
-        //     'X-RapidAPI-Key':'10cd8edd93msh174b0f61d4ea30dp1361a6jsn1dddee813f67'
-        //   })
-        // })
-        // .then(response => response.json())
-        // .then((responseJson) => {
-        //   // TODO: Check movie name matches?
-        //   if (responseJson.results[0].name == this.props.navigation.getParam('title')) {
-        //     sites = responseJson.results[0].locations
-        //     for (let i = 0; i < sites.length; i++) {
-        //       this.setState({
-        //         showingOn: [...this.state.showingOn, {
-        //           url: sites[i].url,
-        //           logo: sites[i].icon,
-        //         }]
-        //       })
-        //     }
-        //   }
-        //   // DEBUG
-        //   console.log(responseJson.results);
-        // });
-        this.setState({
-          showingOn: [...this.state.showingOn, {
-            url: 'https://www.netflix.com/title/80095314',
-            logo: 'https://utellyassets7.imgix.net/locations_icons/utelly/black_new/Netflix.png?auto=compress&app_version=2977d84b-593d-4167-a904-57b1e6e814f1_2019-06-04&w=92',
-          }, {
-            url: null,
-            logo: 'https://utellyassets7.imgix.net/locations_icons/utelly/black_new/Amazon.png?auto=compress&app_version=2977d84b-593d-4167-a904-57b1e6e814f1_2019-06-04&w=92',
-          }, {
-            url: null,
-            logo: 'http://utellyassets7.imgix.net/locations_icons/utelly/live_tv_square/50d352abf0ca9f5798000383.jpg?auto=compress&app_version=2977d84b-593d-4167-a904-57b1e6e814f1_2019-06-04&w=92',
-          }]
+        fetch("https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term="+ this.props.navigation.getParam('title') +"&country=uk", {
+          headers: new Headers({
+            'X-RapidAPI-Host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com',
+            'X-RapidAPI-Key':'10cd8edd93msh174b0f61d4ea30dp1361a6jsn1dddee813f67'
+          })
         })
+        .then(response => response.json())
+        .then((responseJson) => {
+          if (responseJson.results[0].name == this.props.navigation.getParam('title')) {
+            sites = responseJson.results[0].locations
+            for (let i = 0; i < sites.length; i++) {
+              this.setState({
+                showingOn: [...this.state.showingOn, {
+                  url: sites[i].url,
+                  logo: sites[i].icon,
+                }]
+              })
+            }
+          }
+          // DEBUG
+          console.log(responseJson.results);
+        });
+        // this.setState({
+        //   showingOn: [...this.state.showingOn, {
+        //     url: 'https://www.netflix.com/title/80095314',
+        //     logo: 'https://utellyassets7.imgix.net/locations_icons/utelly/black_new/Netflix.png?auto=compress&app_version=2977d84b-593d-4167-a904-57b1e6e814f1_2019-06-04&w=92',
+        //   }, {
+        //     url: null,
+        //     logo: 'https://utellyassets7.imgix.net/locations_icons/utelly/black_new/Amazon.png?auto=compress&app_version=2977d84b-593d-4167-a904-57b1e6e814f1_2019-06-04&w=92',
+        //   }, {
+        //     url: null,
+        //     logo: 'http://utellyassets7.imgix.net/locations_icons/utelly/live_tv_square/50d352abf0ca9f5798000383.jpg?auto=compress&app_version=2977d84b-593d-4167-a904-57b1e6e814f1_2019-06-04&w=92',
+        //   }]
+        // })
       }
 
+    renderStreamingAvailability = () => {
+      if (Object.keys(this.state.showingOn).length === 0) {
+        return (
+          <View style={{paddingBottom: 13}}>
+            <Text>No streaming availability at this time.</Text>
+          </View>
+        )
+      } else {
+        alert(JSON.stringify(this.state.showingOn));
+        return (
+          <View style={{flex: 1, flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center', paddingBottom: 13}}>
+            {this.state.showingOn.map(site => {
+              if (site.url !== null) {
+                return (
+                  <TouchableOpacity onPress={() => Linking.openURL(site.url).catch(error => alert(error))}>
+                    <OriginalSizeImage source={site.logo}/>
+                  </TouchableOpacity>
+                )
+              } else {
+                return (<OriginalSizeImage source={site.logo}/>)
+              }
+            })}
+          </View>
+        )
+      }
+    }
 
     render() {
         return (
@@ -258,19 +284,7 @@ export default class WatchlistMovieScreen extends React.Component {
                 })}
 
                 <Text style={styles.availableOnStyle}>Available on:</Text>
-                <View style={{flex: 1, flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center'}}>
-                  {this.state.showingOn.map(site => {
-                    if (site.url !== null) {
-                      return (
-                        <TouchableOpacity onPress={() => Linking.openURL(site.url).catch(error => alert(error))}>
-                          <OriginalSizeImage source={site.logo}/>
-                        </TouchableOpacity>
-                      )
-                    } else {
-                      return (<OriginalSizeImage source={site.logo}/>)
-                    }
-                  })}
-                </View>
+                {this.renderStreamingAvailability()}
 
             </ScrollView>
             <LinearGradient locations={[0.85, 0.898]} colors={['transparent', '#FFFFFF']} style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0}}/>
